6a04410df0791f79d1d3da442425107b5f7e5783
wojtko <j.w2001@onet.pl>
2023-04-22 17:45:01 +0200

    Make enable to create own NFT

--- src/opend_assets/src/components/App.jsx
+++ src/opend_assets/src/components/App.jsx
@@ -4,14 +4,16 @@ import Footer from "./Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import homeImage from "../../assets/home-img.png";
 import Item from "./Item";
+import Minter from "./Minter";
 
 function App() {
   const nftID = "rrkah-fqaaa-aaaaa-aaaaq-cai";
-  
+
   return (
     <div className="App">
       <Header />
-      <Item id={nftID}/>
+      <Minter />
+      {/* <Item id={nftID}/> */}
       {/* <img className="bottom-space" src={homeImage} /> */}
       <Footer />
     </div>
