ce2472d2cd05486814da02dd3a910ce8241613e7
wojtko <j.w2001@onet.pl>
2023-04-23 18:27:35 +0200

    Make navigation bar works with react-router-dom

--- src/opend_assets/src/components/App.jsx
+++ src/opend_assets/src/components/App.jsx
@@ -2,19 +2,17 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
-import homeImage from "../../assets/home-img.png";
 import Item from "./Item";
 import Minter from "./Minter";
 
 function App() {
-  const nftID = "rrkah-fqaaa-aaaaa-aaaaq-cai";
+  // const nftID = "rrkah-fqaaa-aaaaa-aaaaq-cai";
 
   return (
     <div className="App">
       <Header />
-      <Minter />
+      {/* <Minter /> */}
       {/* <Item id={nftID}/> */}
-      {/* <img className="bottom-space" src={homeImage} /> */}
       <Footer />
     </div>
   );
