b3bcadc84ed488de96501827e55a084bb5eb597a
wojtko <j.w2001@onet.pl>
2023-04-28 18:22:07 +0200

    Creating the Discover page

--- src/opend/main.mo
+++ src/opend/main.mo
@@ -5,6 +5,7 @@ import Cycles "mo:base/ExperimentalCycles";
 import Debug "mo:base/Debug";
 import HashMap "mo:base/HashMap";
 import List "mo:base/List";
+import Iter "mo:base/Iter";
 
 actor OpenD {
 
@@ -49,6 +50,11 @@ actor OpenD {
         };
 
         return List.toArray(userNFTs);
+    };
+
+     public query func getListedNFTs() : async [Principal] {
+        let ids = Iter.toArray(mapOfListings.keys());
+        return ids;
     };
 
     public shared (msg) func listItem(id : Principal, price : Nat) : async Text {
--- src/opend_assets/src/components/Header.jsx
+++ src/opend_assets/src/components/Header.jsx
@@ -9,11 +9,14 @@ import CURRENT_USER_ID from "../index";
 
 function Header() {
   const [userOwnedGallery, setOwnedGallery] = useState();
+  const [listingGallery, setListingGallery] = useState();
 
   async function getNFTs() {
     const userNFTIds = await opend.getOwnedNFTs(CURRENT_USER_ID);
-    // console.log(userNFTIds);
     setOwnedGallery(<Gallery title="My NFTs" ids={userNFTIds} />);
+
+    const listenedNFTs = await opend.getListedNFTs();
+    setListingGallery(<Gallery title="Discover" ids={listenedNFTs} />);
   }
 
   useEffect(() => {
@@ -50,7 +53,8 @@ function Header() {
           <img className="bottom-space" src={homeImage} />
         </Route>
         <Route path="/discover">
-          <h1>Discover</h1>
+          {listingGallery}
+          {/* <h1>Discover</h1> */}
         </Route>
         <Route path="/minter">
           <Minter />
